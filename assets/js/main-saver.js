b7c8869a32dc11cebe7bcf50f88150690e1a55cf
mclvren <aiv-n@ya.ru>
2019-04-23 21:16:36 +0300

    filesaver

--- assets/js/main-saver.js
+++ assets/js/main-saver.js
@@ -1,3 +1,5 @@
+import { saveAs } from 'file-saver';
+FileSaver saveAs(Blob/File/Url, optional DOMString filename, optional Object { autoBOM: true })
 $("#save-btn").click(function() {
   var file = new File(["Hello, world!"], "hello world.txt", {type: "text/plain;charset=utf-8"});
   FileSaver.saveAs(file);
