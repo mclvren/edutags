5d91e9ebae1a5dc6ad05f9a6ad71a3cb895a0720
mclvren <aiv-n@ya.ru>
2019-04-23 21:28:03 +0300

    1

--- assets/js/main-saver.js
+++ assets/js/main-saver.js
@@ -1,5 +1,4 @@
 $("#save-btn").click(function() {
-  var FileSaver = require('file-saver');
   var file = new File(["Hello, world!"], "hello world.txt", {type: "text/plain;charset=utf-8"});
-  FileSaver.saveAs(file);
+  saveAs(file);
 });
