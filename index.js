02bfd65c185cdf2185a2436b499f6363a1861144
mclvren <aiv-n@ya.ru>
2019-05-12 15:28:10 +0300

    node init

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
    res.send('Hello World');
 })
@@ -10,4 +12,5 @@ var server = app.listen(8081, function () {
    var port = server.address().port
 
    console.log("Example app listening at http://%s:%s", host, port)
+
 })
