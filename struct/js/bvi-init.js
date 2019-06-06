4893b5e310249f7ff07ced7704d0a53910892f7c
mclvren <aiv-n@ya.ru>
2019-06-06 22:14:09 +0300

    fix test pages

---
+++ struct/js/bvi-init.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function($){
+    $.bvi({
+            'bvi_target': '.bvi-open',
+            "bvi_theme":"white",
+            "bvi_font":"arial",
+            "bvi_font_size":16,
+            "bvi_letter_spacing":"normal",
+            "bvi_line_height":"normal",
+            "bvi_images":false,
+            "bvi_reload":false,
+            "bvi_fixed":true,
+            "bvi_voice":true,
+            "bvi_flash_iframe":true,
+            "bvi_hide":false
+    });
+});
