a3565217b006a75607f07a2395d6c6cd5bb69224
mclvren <aiv-n@ya.ru>
2019-06-14 13:03:20 +0300

    docs

--- modules/zipDocs.js
+++ modules/zipDocs.js
@@ -2,9 +2,9 @@
 const far_js = require('./far.js');
 //Функция поиска и замены
 findAndReplace = far_js.findAndReplace;
-module.exports.func = function(app, db, fs, h, path, zip, others_n, kaf_n, stud_n, pub_n) {
+module.exports.func = function(app, db, fs, h, path, zip, docs_n) {
   app.get("/saveDocs_zip", function(req, res) {
-    var html = fs.readFileSync(__dirname+"/templates/docs/docs.html", {
+    var html = fs.readFileSync(__dirname+"/templates/docs/document.html", {
       encoding: "utf8"
     });
     var json = h.parse(html);
@@ -17,84 +17,77 @@ module.exports.func = function(app, db, fs, h, path, zip, others_n, kaf_n, stud_
       .value();
     // Замена полей в шаблоне
     function FAR(json) {
-      //Адреса
-      findAndReplace(json, "*struc_address*", values.controls_0_name);
       //Иные
-      for (var i = 0; i < others_n; i++) {
+      for (var i = 0; i < docs_n; i++) {
         n = i + 1;
-        findAndReplace(json, "*others_name_" + n + "*", eval("values.departments_others_" + i + "_name"));
-        findAndReplace(json, "*others_fio_" + n + "*", eval("values.departments_others_" + i + "_fio"));
-        findAndReplace(json, "*others_post_" + n + "*", eval("values.departments_others_" + i + "_post"));
-        findAndReplace(json, "*others_address_" + n + "*", eval("values.departments_others_" + i + "_address_str"));
-        findAndReplace(json, "*others_site_" + n + "*", eval("values.departments_others_" + i + "_site"));
-        findAndReplace(json, "*others_email_" + n + "*", eval("values.departments_others_" + i + "_email"));
-        findAndReplace(json, "*others_doc_link_" + n + "*", eval("values.departments_others_" + i + "_division_clause_doc_link"));
-        findAndReplace(json, "*others_phone_" + n + "*", eval("values.departments_others_" + i + "_phone"));
-      }
-      //Кафедры
-      for (var i = 0; i < kaf_n; i++) {
-        n = i + 1;
-        findAndReplace(json, "*kaf_name_" + n + "*", eval("values.departments_" + i + "_name"));
-        findAndReplace(json, "*kaf_fio_" + n + "*", eval("values.departments_" + i + "_fio"));
-        findAndReplace(json, "*kaf_post_" + n + "*", eval("values.departments_" + i + "_post"));
-        findAndReplace(json, "*kaf_address_" + n + "*", eval("values.departments_" + i + "_address_str"));
-        findAndReplace(json, "*kaf_site_" + n + "*", eval("values.departments_" + i + "_site"));
-        findAndReplace(json, "*kaf_email_" + n + "*", eval("values.departments_" + i + "_email"));
-        findAndReplace(json, "*kaf_doc_link_" + n + "*", eval("values.departments_" + i + "_division_clause_doc_link"));
-      }
-      //Студенческие органы управления, объединения, организации
-      for (var i = 0; i < stud_n; i++) {
-        n = i + 1;
-        findAndReplace(json, "*stud_name_" + n + "*", eval("values.departments_st_" + i + "_name"));
-        findAndReplace(json, "*stud_fio_" + n + "*", eval("values.departments_st_" + i + "_fio"));
-        findAndReplace(json, "*stud_post_" + n + "*", eval("values.departments_st_" + i + "_post"));
-        findAndReplace(json, "*stud_address_" + n + "*", eval("values.departments_st_" + i + "_address_str"));
-        findAndReplace(json, "*stud_site_" + n + "*", eval("values.departments_st_" + i + "_site"));
-        findAndReplace(json, "*stud_email_" + n + "*", eval("values.departments_st_" + i + "_email"));
-        findAndReplace(json, "*stud_doc_link_" + n + "*", eval("values.departments_st_" + i + "_division_clause_doc_link"));
-      }
-      //Общественные организации
-      for (var i = 0; i < pub_n; i++) {
-        n = i + 1;
-        findAndReplace(json, "*pub_name_" + n + "*", eval("values.departments_public_" + i + "_name"));
-        findAndReplace(json, "*pub_fio_" + n + "*", eval("values.departments_public_" + i + "_fio"));
-        findAndReplace(json, "*pub_post_" + n + "*", eval("values.departments_public_" + i + "_post"));
-        findAndReplace(json, "*pub_address_" + n + "*", eval("values.departments_public_" + i + "_address_str"));
-        findAndReplace(json, "*pub_site_" + n + "*", eval("values.departments_public_" + i + "_site"));
-        findAndReplace(json, "*pub_email_" + n + "*", eval("values.departments_public_" + i + "_email"));
-        findAndReplace(json, "*pub_doc_link_" + n + "*", eval("values.departments_public_" + i + "_division_clause_doc_link"));
+        findAndReplace(json, "*ustavs_" + n + "_name*", eval("values.ustavs_" + i + "_name"));
+        findAndReplace(json, "*ustavs_" + n + "_url*", eval("values.ustavs_" + i + "_url"));
+        findAndReplace(json, "*licenses_" + n + "_name*", eval("values.licenses_" + i + "_name"));
+        findAndReplace(json, "*licenses_" + n + "_url*", eval("values.licenses_" + i + "_url"));
+        findAndReplace(json, "*accreditations_" + n + "_name*", eval("values.accreditations_" + i + "_name"));
+        findAndReplace(json, "*accreditations_" + n + "_url*", eval("values.accreditations_" + i + "_url"));
+        findAndReplace(json, "*fin_plans_" + n + "_name*", eval("values.fin_plans_" + i + "_name"));
+        findAndReplace(json, "*fin_plans_" + n + "_url*", eval("values.fin_plans_" + i + "_url"));
+        findAndReplace(json, "*priems_" + n + "_name*", eval("values.priems_" + i + "_name"));
+        findAndReplace(json, "*priems_" + n + "_url*", eval("values.priems_" + i + "_url"));
+        findAndReplace(json, "*modes_" + n + "_name*", eval("values.modes_" + i + "_name"));
+        findAndReplace(json, "*modes_" + n + "_url*", eval("values.modes_" + i + "_url"));
+        findAndReplace(json, "*tek_kontrols_" + n + "_name*", eval("values.tek_kontrols_" + i + "_name"));
+        findAndReplace(json, "*tek_kontrols_" + n + "_url*", eval("values.tek_kontrols_" + i + "_url"));
+        findAndReplace(json, "*perevods_" + n + "_name*", eval("values.perevods_" + i + "_name"));
+        findAndReplace(json, "*perevods_" + n + "_url*", eval("values.perevods_" + i + "_url"));
+        findAndReplace(json, "*vozes_" + n + "_name*", eval("values.vozes_" + i + "_name"));
+        findAndReplace(json, "*vozes_" + n + "_url*", eval("values.vozes_" + i + "_url"));
+        findAndReplace(json, "*local_act_studs_" + n + "_name*", eval("values.local_act_studs_" + i + "_name"));
+        findAndReplace(json, "*local_act_studs_" + n + "_url*", eval("values.local_act_studs_" + i + "_url"));
+        findAndReplace(json, "*local_act_orders_" + n + "_name*", eval("values.local_act_orders_" + i + "_name"));
+        findAndReplace(json, "*local_act_orders_" + n + "_url*", eval("values.local_act_orders_" + i + "_url"));
+        findAndReplace(json, "*local_act_collecs_" + n + "_name*", eval("values.local_act_collecs_" + i + "_name"));
+        findAndReplace(json, "*local_act_collecs_" + n + "_url*", eval("values.local_act_collecs_" + i + "_url"));
+        findAndReplace(json, "*report_edus_" + n + "_name*", eval("values.report_edus_" + i + "_name"));
+        findAndReplace(json, "*report_edus_" + n + "_url*", eval("values.report_edus_" + i + "_url"));
+        findAndReplace(json, "*paid_edus_" + n + "_name*", eval("values.paid_edus_" + i + "_name"));
+        findAndReplace(json, "*paid_edus_" + n + "_url*", eval("values.paid_edus_" + i + "_url"));
+        findAndReplace(json, "*paid_edu_dogs_" + n + "_name*", eval("values.paid_edu_dogs_" + i + "_name"));
+        findAndReplace(json, "*paid_edu_dogs_" + n + "_url*", eval("values.paid_edu_dogs_" + i + "_url"));
+        findAndReplace(json, "*paid_edu_stoims_" + n + "_name*", eval("values.paid_edu_stoims_" + i + "_name"));
+        findAndReplace(json, "*paid_edu_stoims_" + n + "_url*", eval("values.paid_edu_stoims_" + i + "_url"));
+        findAndReplace(json, "*prescriptions_" + n + "_name*", eval("values.prescriptions_" + i + "_name"));
+        findAndReplace(json, "*prescriptions_" + n + "_url*", eval("values.prescriptions_" + i + "_url"));
+        findAndReplace(json, "*prescription_otchets_" + n + "_name*", eval("values.prescription_otchets_" + i + "_name"));
+        findAndReplace(json, "*prescription_otchets_" + n + "_url*", eval("values.prescription_otchets_" + i + "_url"));
       }
       return json;
     }
-    struct = h.stringify(FAR(json));
+    document = h.stringify(FAR(json));
     res
       .zip({
         files: [{
-            content: struct,
-            name: "/struct/index.php",
+            content: document,
+            name: "/document/index.php",
             mode: 0755,
             comment: "structure",
             date: new Date(),
             type: "file"
           },
           {
-            path: path.join(__dirname, "/templates/struct/css"),
-            name: "/struct/css"
+            path: path.join(__dirname, "/templates/docs/css"),
+            name: "/document/css"
           },
           {
-            path: path.join(__dirname, "/templates/struct/fonts"),
-            name: "/struct/fonts"
+            path: path.join(__dirname, "/templates/docs/fonts"),
+            name: "/document/fonts"
           },
           {
-            path: path.join(__dirname, "/templates/struct/js"),
-            name: "/struct/js"
+            path: path.join(__dirname, "/templates/docs/js"),
+            name: "/document/js"
           }
         ],
-        filename: "structure.zip"
+        filename: "document.zip"
       })
       .catch(function(err) {
         res.send(err); //if zip failed
       });
-    console.log("structure.zip сгенерирован и отправлен");
+    console.log("document.zip сгенерирован и отправлен");
   });
 }
